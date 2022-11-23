a9375187a846a3b5290c3d2425f3150ffc3973c0
gilmutdinoff.serj@yandex.ru <gilmutdinoff.serj@yandex.ru>
2022-11-23 22:47:34 +0500

    Create Worker Struct

---
+++ practice_70/Worker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_70
+{
+    struct Worker
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public int Age { get; set; }
+        public int Height { get; set; }
+        public DateTime BirthDay { get; set; } 
+        public string BirthPlace { get; set; }
+
+    }
+}
