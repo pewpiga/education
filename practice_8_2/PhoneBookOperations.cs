98610d33545f738d3bb315f36d9aeb24286183da
gilmutdinoff.serj@yandex.ru <gilmutdinoff.serj@yandex.ru>
2022-12-08 22:46:37 +0500

    .

---
+++ practice_8_2/PhoneBookOperations.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace practice_8_2
+{
+    class PhoneBookOperations
+    {
+        private Dictionary<string, string> phoneBook;
+        public PhoneBookOperations()
+        {
+            this.phoneBook = new Dictionary<string, string>();
+        }
+        public void StartProgram()
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.Write("1 - Ввод данных;\n" +
+                          "2 - Поиск данных;\n" +
+                          "3 - Выход;\n" +
+                          "Ввод: ");
+
+                int id = Convert.ToInt32(Console.ReadLine());
+
+                switch (id)
+                {
+                    case 1:
+                        InputData();
+                        break;
+                    case 2:
+                        OutputData();
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                }
+            }            
+        }
+        private void InputData()
+        {
+            string inputPhone;
+            string inputName;
+
+            while (true)
+            {
+                Console.Write("Введите телефон: ");
+                inputPhone = Console.ReadLine();
+
+                if (inputPhone == "") break;
+
+                Console.Write("Введите имя: ");
+                inputName = Console.ReadLine();
+
+                phoneBook.Add(inputPhone, inputName);
+            }
+        }
+        private void OutputData() 
+        {
+            string inputPhone;
+            string name;
+
+            Console.Write("Введите телефон: ");
+            inputPhone = Console.ReadLine();
+
+            foreach (var key in phoneBook.Keys)
+            {
+                if (key == inputPhone)
+                {
+                    Console.WriteLine("Владелец данного номера: {0}", name = phoneBook[key]);
+                }
+            }
+        }
+    }
+}
--- practice_8_2/Program.cs
+++ practice_8_2/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿using practice_8_2;
+
+PhoneBookOperations bookOperations = new PhoneBookOperations();
+
+bookOperations.StartProgram();
